5eaeb0ce3d67df740048ef8a7366c843e1e71e76
noasakurajin <noasakurajin@web.de>
2020-05-02 07:19:55 +0200

    allowing to leave hometown

--- elle_hometwon/ModEntry.cs
+++ elle_hometwon/ModEntry.cs
@@ -5,33 +5,31 @@ using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
 
-namespace CallMeElle
+namespace CallMeElle.hometown
 {
-    namespace hometown
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
     {
-        /// <summary>The mod entry point.</summary>
-        public class ModEntry : Mod
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
         {
-            /*********
-            ** Public methods
-            *********/
-            /// <summary>The mod entry point, called after the mod is first loaded.</summary>
-            /// <param name="helper">Provides simplified APIs for writing mods.</param>
-            public override void Entry(IModHelper helper)
-            {
-                helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
-            }
-
-            private void OnSaveLoaded(object sender, SaveLoadedEventArgs args)
-            {
-                // get the internal asset key for the map file
-                string mapAssetKey = this.Helper.Content.GetActualAssetKey("assets/parents.tmx", ContentSource.ModFolder);
-
-                // add the location
-                GameLocation location = new GameLocation(mapAssetKey, "Hometown") { IsOutdoors = true, IsFarm = false };
-                Game1.locations.Add(location);
-            }
-
+            helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
         }
+
+        private void OnSaveLoaded(object sender, SaveLoadedEventArgs args)
+        {
+           // get the internal asset key for the map file
+			string mapAssetKey = this.Helper.Content.GetActualAssetKey("assets/parents.tmx", ContentSource.ModFolder);
+
+			// add the location
+			GameLocation location = new Hometown(mapAssetKey);
+			Game1.locations.Add(location);
+        }
+
     }
+    
 }
---
+++ elle_hometwon/hometown.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace CallMeElle.hometown {
+	public class Hometown : GameLocation {
+
+		public Hometown( string mapAssetKey ) : base(mapAssetKey, "Hometown") {
+			IsOutdoors = true;
+			IsFarm = false;
+		}
+
+		public override void performTouchAction( string fullActionString, Vector2 playerStandingPosition ) {
+			string text = fullActionString.Split(' ')[0];
+			if ( text != null && text == "DesertBus" ) {
+				Response[] answerChoices = {
+					new Response("Yes", Game1.content.LoadString("Strings\\Locations:Desert_Return_Yes")),
+					new Response("Not", Game1.content.LoadString("Strings\\Locations:Desert_Return_No"))
+				};
+				createQuestionDialogue(Game1.content.LoadString("Strings\\Locations:Desert_Return_Question"), answerChoices, "DesertBus");
+			} else {
+				base.performTouchAction(fullActionString, playerStandingPosition);
+			}
+		}
+
+		private void playerReachedBusDoor( Character c, GameLocation l ) {
+			Game1.player.position.X = -10000f;
+			Game1.freezeControls = true;
+			Game1.player.CanMove = false;
+			busDriveOff();
+			playSound("stoneStep");
+		}
+
+		public override bool answerDialogue( Response answer ) {
+			if ( lastQuestionKey != null && afterQuestion == null ) {
+				string text = lastQuestionKey.Split(' ')[0] + "_" + answer.responseKey;
+				if ( text != null && text == "DesertBus_Yes" ) {
+					//playerReachedBusDoor(Game1.player, this);
+					warpHome();
+					return true;
+				}
+			}
+			return base.answerDialogue(answer);
+		}
+
+		public void warpHome() {
+			/*Game1.globalFadeToBlack(delegate {
+				Game1.globalFadeToClear();
+				localSound("batFlap");
+				//drivingOff = true;
+				localSound("busDriveOff");
+				Game1.changeMusicTrack("none");
+			});*/
+			//Game1.viewportFreeze = true;
+			Game1.warpFarmer("BusStop", 12, 10, flip: true);
+		}
+
+		public void busDriveOff() {
+			/*busDoor = new TemporaryAnimatedSprite("LooseSprites\\Cursors", new Microsoft.Xna.Framework.Rectangle(288, 1311, 16, 38), busPosition + new Vector2(16f, 26f) * 4f, flipped: false, 0f, Color.White) {
+				interval = 999999f,
+				animationLength = 6,
+				holdLastFrame = true,
+				layerDepth = ( busPosition.Y + 192f ) / 10000f + 1E-05f,
+				scale = 4f
+			};
+			busDoor.timer = 0f;
+			busDoor.interval = 70f;
+			busDoor.endFunction = busStartMovingOff;*/
+			busStartMovingOff(0);		
+			/*localSound("trashcanlid");
+			drivingBack = false;
+			busDoor.paused = false;*/
+		}
+
+		private void busStartMovingOff( int extraInfo ) {
+		
+		}
+
+	}
+}
