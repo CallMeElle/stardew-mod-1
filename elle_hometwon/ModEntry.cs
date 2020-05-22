ef0a3cb40fa1de0eb4df862b59522ff002cdce75
noasakurajin <noasakurajin@web.de>
2020-05-22 14:33:51 +0200

    small map improvements and code reformatting

--- elle_hometwon/ModEntry.cs
+++ elle_hometwon/ModEntry.cs
@@ -8,52 +8,51 @@ using StardewValley;
 namespace CallMeElle.hometown
 {
     /// <summary>The mod entry point.</summary>
-    public class ModEntry : Mod
-    {
-		private Hometown location;
-		/*********
+    public class ModEntry : Mod{
+
+        private Hometown location;
+
+        /*********
         ** Public methods
         *********/
-		/// <summary>The mod entry point, called after the mod is first loaded.</summary>
-		/// <param name="helper">Provides simplified APIs for writing mods.</param>
-		public override void Entry(IModHelper helper)
-        {
-			helper.Events.GameLoop.DayEnding += this.BeforeSaved;
-			helper.Events.GameLoop.DayStarted += this.AfterSaved;
-		}
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper){
+            //Multiplayer multiplayer = this.Helper.Reflection.GetField<Multiplayer>(typeof(Game1), "multiplayer").GetValue();
+
+            helper.Events.GameLoop.DayEnding += this.BeforeSaved;
+            helper.Events.GameLoop.DayStarted += this.AfterSaved;
+        }
         
 
-		private void BeforeSaved( object sender, DayEndingEventArgs args) {
-			// add the location
+        private void BeforeSaved( object sender, DayEndingEventArgs args) {
+            // add the location
 
-			if ( Game1.locations.Contains(location) ) {
-				Game1.locations.Remove(location);
-				this.Monitor.Log("removed hometown", LogLevel.Info);
-			} else {
-				this.Monitor.Log("cannot find hometown", LogLevel.Warn);
-			}
+            if ( Game1.locations.Contains(location) ) {
+            	Game1.locations.Remove(location);
+            	this.Monitor.Log("removed hometown", LogLevel.Info);
+            } else {
+            	this.Monitor.Log("cannot find hometown", LogLevel.Warn);
+            }
 
-		}
+        }
 
-		private void AfterSaved( object sender, DayStartedEventArgs args ) {
-			// add the location
+        private void AfterSaved( object sender, DayStartedEventArgs args ) {
+            // add the location
 
-			if ( location == null ) {
-				location = new Hometown(this.Helper);
-				this.Monitor.Log("created hometown", LogLevel.Info);
-			}
+            if ( location == null ) {
+            	location = new Hometown(this.Helper);
+            	this.Monitor.Log("created hometown", LogLevel.Info);
+            }
 
-			if( Game1.locations.Contains(location) ) {
-				this.Monitor.Log("hometown already added", LogLevel.Error);
-			} else {
-				Game1.locations.Add(location);
-				this.Monitor.Log("added hometown", LogLevel.Info);
-			}
+            if( Game1.locations.Contains(location) ) {
+            	this.Monitor.Log("hometown already added", LogLevel.Error);
+            } else {
+            	Game1.locations.Add(location);
+            	this.Monitor.Log("added hometown", LogLevel.Info);
+            }
 
-
-
-		}
-
+        }
 
 
 	}
